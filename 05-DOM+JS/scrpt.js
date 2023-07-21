b4fac5e5d0ef7ee88e94403042e1ae5e7fd9c97e
rahul8899 <rahulkaklotar00@gmail.com>
2023-07-21 18:42:21 +0530

    Js and DOM

---
+++ 05-DOM+JS/scrpt.js
@@ -0,0 +1,12 @@
+// function clicked() {
+//     console.log("you clicked on page");
+     
+// }
+
+// document.addEventListener('click',clicked);
+
+let content = document.querySelector('#name');
+
+document.addEventListener('click',function clicked(event) {
+    console.log(event)
+})
